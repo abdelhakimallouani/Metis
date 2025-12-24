bf53f5eda3090b18411bd2b95cfd34ec9d07752a
abdelhakim allouani <abdelhakimallouani@gmail.com>
2025-12-24 09:12:01 +0100

    feat (config,app/Database): ajouter la configuration et la connections de base de donnes via le PDO et Singleton

---
+++ app/Database/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database
+{
+    private static $instance = null;
+    private $connection;
+
+    private function __construct()
+    {
+        $config = require __DIR__ . '/../../config/database.php';
+
+        $this->connection = new PDO("mysql:host={$config['host']};dbname={$config['dbname']}", $config['user'], $config['password']);
+
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'dbname' => 'metis',
+    'user' => 'root',
+    'password' => ''
+];
+?>
